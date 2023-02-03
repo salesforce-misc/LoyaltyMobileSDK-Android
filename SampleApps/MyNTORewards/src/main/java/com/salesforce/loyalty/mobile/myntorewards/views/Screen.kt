b39e856945cabe9cd830af3a32e1f22a595bb569
akash.agrawal <akash.agrawal@salesforce.com>
2023-02-03 12:02:04 +0530

    updated navigation

---
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/Screen.kt
@@ -0,0 +1,9 @@
+package com.salesforce.loyalty.mobile.myntorewards.views
+
+sealed class Screen(val route:String)
+{
+    object OnboardingScreen: Screen("onboarding_screen")
+    object HomeScreen: Screen("home_screen")
+    object LoginScreen: Screen("login_screen")
+
+}
