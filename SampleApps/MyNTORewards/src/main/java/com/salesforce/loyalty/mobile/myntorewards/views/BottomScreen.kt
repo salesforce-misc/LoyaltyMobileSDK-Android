edb993a7ecdd873628402897f7b0672bf9c3c1b8
akash.agrawal <akash.agrawal@salesforce.com>
2023-02-08 11:53:43 +0530

    Bottom tab bar

---
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/BottomScreen.kt
@@ -0,0 +1,114 @@
+package com.salesforce.loyalty.mobile.myntorewards.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.salesforce.loyalty.mobile.MyNTORewards.R
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.TextPurpoleLightBG
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TextPurpoleLightBG)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Home Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun MyOfferScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TextPurpoleLightBG)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "My Offers Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun MyProfileScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TextPurpoleLightBG)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Add Profile Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+
+@Composable
+fun RedeemScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TextPurpoleLightBG)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Redeem Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+
+@Composable
+fun MoreScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(TextPurpoleLightBG)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "More Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
