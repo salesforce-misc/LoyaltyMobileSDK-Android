3ba055d03f2f7ebe7550e61fe0a63661da83a2cd
akash.agrawal <akash.agrawal@salesforce.com>
2023-04-25 09:58:54 +0530

    1- Fixed scrolling cache and bug fix.

--- SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/viewmodels/VoucherViewModel.kt
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/viewmodels/VoucherViewModel.kt
@@ -8,10 +8,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.salesforce.loyalty.mobile.myntorewards.forceNetwork.ForceAuthManager
 import com.salesforce.loyalty.mobile.myntorewards.utilities.AppConstants
+import com.salesforce.loyalty.mobile.myntorewards.utilities.LocalFileManager
+import com.salesforce.loyalty.mobile.myntorewards.viewmodels.viewStates.PromotionViewState
+import com.salesforce.loyalty.mobile.myntorewards.viewmodels.viewStates.VoucherViewState
 import com.salesforce.loyalty.mobile.sources.PrefHelper
 import com.salesforce.loyalty.mobile.sources.PrefHelper.get
 import com.salesforce.loyalty.mobile.sources.loyaltyAPI.LoyaltyAPIManager
+import com.salesforce.loyalty.mobile.sources.loyaltyModels.PromotionsResponse
 import com.salesforce.loyalty.mobile.sources.loyaltyModels.VoucherResponse
+import com.salesforce.loyalty.mobile.sources.loyaltyModels.VoucherResult
 import kotlinx.coroutines.launch
 
 class VoucherViewModel : ViewModel() {
@@ -27,7 +32,34 @@ class VoucherViewModel : ViewModel() {
     private val vouchers = MutableLiveData<List<VoucherResponse>>()
 
 
+    val voucherViewState: LiveData<VoucherViewState>
+        get() = viewState
+
+    private val viewState = MutableLiveData<VoucherViewState>()
+
+    fun loadVoucher(context: Context) {
+        viewState.postValue(VoucherViewState.VoucherFetchInProgress)
+        viewModelScope.launch {
+            val membershipKey =
+                PrefHelper.customPrefs(context)[AppConstants.KEY_MEMBERSHIP_NUMBER, ""] ?: ""
+            val voucherCache = LocalFileManager.getData(
+                context,
+                membershipKey,
+                LocalFileManager.DIRECTORY_VOUCHERS,
+                VoucherResult::class.java
+            )
+
+            Log.d(TAG, "cache : $voucherCache")
+            if (voucherCache == null) {
+                getVoucher(context)
+            } else {
+                vouchers.value = voucherCache.voucherResponse
+                viewState.postValue(VoucherViewState.VoucherFetchSuccess)
+            }
+        }
+    }
     fun getVoucher(context: Context) {
+        viewState.postValue(VoucherViewState.VoucherFetchInProgress)
         viewModelScope.launch {
             var memberID = PrefHelper.customPrefs(context)[AppConstants.KEY_PROGRAM_MEMBER_ID, ""]
             var membershipKey =
@@ -37,9 +69,18 @@ class VoucherViewModel : ViewModel() {
                     membershipKey, null, 1, null,
                     null, null, null
                 ).onSuccess {
+                    LocalFileManager.saveData(
+                        context,
+                        it,
+                        membershipKey,
+                        LocalFileManager.DIRECTORY_VOUCHERS
+                    )
+
                     vouchers.value = it.voucherResponse
+                    viewState.postValue(VoucherViewState.VoucherFetchSuccess)
                     Log.d(TAG, "getVoucher success: ${it.voucherResponse}")
                 }.onFailure {
+                    viewState.postValue(VoucherViewState.VoucherFetchFailure)
                     Log.d(TAG, "getVoucher failed: ${it.message}")
                 }
             }
