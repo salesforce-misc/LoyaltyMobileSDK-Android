55a74e5df1b371b33bba9987cc42ac281f1e925e
Nirupama Natarajan <nirupama.natarajan@salesforce.com>
2023-04-28 12:14:29 +0530

    Add OSS Template files and Copyright Headers

--- sources/src/main/java/com/salesforce/loyalty/mobile/sources/forceUtils/ForceAuthenticator.kt
+++ sources/src/main/java/com/salesforce/loyalty/mobile/sources/forceUtils/ForceAuthenticator.kt
@@ -1,11 +1,14 @@
+/*
+ * Copyright (c) 2023, Salesforce, Inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 package com.salesforce.loyalty.mobile.sources.forceUtils
 
-import com.salesforce.loyalty.mobile.sources.forceModels.ForceAuthResponse
-
 interface ForceAuthenticator {
-
     fun getAccessToken(): String?
-
     suspend fun grantAccessToken(): String?
 
 }
