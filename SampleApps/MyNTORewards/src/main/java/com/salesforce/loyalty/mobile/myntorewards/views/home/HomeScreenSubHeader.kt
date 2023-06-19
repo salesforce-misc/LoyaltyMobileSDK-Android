4cb9bff0f68d533e7ecd7234d01ea7cba9f1338a
akash.agrawal <akash.agrawal@salesforce.com>
2023-06-19 16:02:01 +0530

    Bug fix Navigation from ViewAll--> Promotion--HomeScreen.

--- SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/home/HomeScreenSubHeader.kt
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/home/HomeScreenSubHeader.kt
@@ -44,17 +44,7 @@ fun HomeSubViewHeader(headingId: Int, bottomTabsNavController: NavController) {
             textAlign = TextAlign.Center,
             fontSize = 13.sp,
             modifier = Modifier.clickable {
-                bottomTabsNavController.navigate(BottomNavTabs.MyOffers.route) {
-
-                    bottomTabsNavController.graph.startDestinationRoute?.let { screen_route ->
-                        popUpTo(screen_route) {
-                            saveState = true
-                        }
-                    }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            }
+                bottomTabsNavController.navigate(BottomNavTabs.MyOffers.route) }
         )
-    }
+}
 }
