f76bd15d26db8f46005eb9e008f5759923824970
Nirupama Natarajan <nirupama.natarajan@salesforce.com>
2023-07-18 23:06:21 +0530

    Receipt Scanning - Post scanning screen

---
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/receipts/ReceiptTable.kt
@@ -0,0 +1,160 @@
+package com.salesforce.loyalty.mobile.myntorewards.views.receipts
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.font_ibm_plex_mono_regular
+
+data class Invoice(val itemName: String, val qty: String, val price: String, val total: String)
+
+@Composable
+fun ReceiptDetailTable() {
+
+    val invoiceList = listOf(
+        Invoice("Converse Shoes", "1", "\$599", "\$599"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199"),
+        Invoice("Converse Socks", "1", "\$199", "\$199")
+    )
+
+    val column1Weight = 0.4f
+    val column2Weight = 0.15f
+    val column3Weight = 0.2f
+    val column4Weight = 0.2f
+
+    LazyColumn(
+        Modifier
+            .padding(top = 16.dp)
+            .background(color = Color.White, shape = RectangleShape)
+            .padding(8.dp)) {
+        item {
+            Spacer(modifier = Modifier.height(8.dp))
+            DrawDashLine()
+            Spacer(modifier = Modifier.height(4.dp))
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                TableCell(
+                    text = "Item Name",
+                    weight = column1Weight,
+                    alignment = TextAlign.Left,
+                    title = true
+                )
+                TableCell(
+                    text = "Qty",
+                    weight = column2Weight,
+                    alignment = TextAlign.Left,
+                    title = true
+                )
+                TableCell(
+                    text = "Price",
+                    weight = column3Weight,
+                    alignment = TextAlign.Left,
+                    title = true
+                )
+                TableCell(
+                    text = "Total",
+                    weight = column4Weight,
+                    alignment = TextAlign.Right,
+                    title = true
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            DrawDashLine()
+            Spacer(modifier = Modifier.height(4.dp))
+        }
+        itemsIndexed(invoiceList) { index, invoice ->
+            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                TableCell(
+                    text = invoice.itemName,
+                    weight = column1Weight,
+                    alignment = TextAlign.Left
+                )
+                TableCell(
+                    text = invoice.qty,
+                    weight = column2Weight,
+                    alignment = TextAlign.Left
+                )
+                TableCell(
+                    text = invoice.price,
+                    weight = column3Weight,
+                    alignment = TextAlign.Left
+                )
+                TableCell(
+                    text = invoice.total,
+                    weight = column4Weight,
+                    alignment = TextAlign.Right
+                )
+            }
+            if (index == invoiceList.size - 1) {
+                Spacer(modifier = Modifier.height(8.dp))
+                DrawDashLine()
+                Spacer(modifier = Modifier.height(150.dp))
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun RowScope.TableCell(
+    text: String,
+    weight: Float,
+    alignment: TextAlign = TextAlign.Center,
+    title: Boolean = false
+) {
+    Text(
+        text = text,
+        modifier = if (title) Modifier
+            .weight(weight)
+            .padding(5.dp) else Modifier.weight(weight).padding(start = 5.dp, end = 5.dp),
+        fontWeight = if (title) FontWeight.Bold else FontWeight.Normal,
+        style = TextStyle(
+            fontSize = 10.sp,
+            lineHeight = 14.sp,
+            fontFamily = font_ibm_plex_mono_regular,
+            fontWeight = FontWeight(400),
+            color = Color(0xFF000000),
+        ),
+        textAlign = alignment
+    )
+}
+
+@Composable
+private fun DrawDashLine() {
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(20f, 10f), 0f)
+    Canvas(
+        Modifier.fillMaxWidth()
+    ) {
+        drawLine(
+            color = Color(0xFF000000),
+            strokeWidth = 5f,
+            start = Offset(20f, 0f),
+            end = Offset(size.width - 20, 0f),
+            colorFilter = ColorFilter.tint(color = Color.Black),
+            pathEffect = pathEffect
+        )
+    }
+}
---
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/receipts/ScannedReceipt.kt
@@ -0,0 +1,136 @@
+package com.salesforce.loyalty.mobile.myntorewards.views.receipts
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+import com.salesforce.loyalty.mobile.MyNTORewards.R
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.LighterBlack
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.MyProfileScreenBG
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.VibrantPurple40
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.font_sf_pro
+
+@Composable
+fun ShowScannedReceiptPopup() {
+    Popup(
+        alignment = Alignment.Center,
+        offset = IntOffset(0, 800),
+        onDismissRequest = {/*closePopup() */ },
+        properties = PopupProperties(
+            focusable = true,
+            dismissOnBackPress = true,
+            dismissOnClickOutside = false
+        ),
+    ) {
+        Box() {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.9f)
+                    .background(MyProfileScreenBG, RoundedCornerShape(22.dp))
+                    .padding(16.dp)
+                    /*.verticalScroll(
+                        rememberScrollState()
+                    )*/,
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.label_receipt_number) + " " + "2323",
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 20.sp,
+                        fontFamily = font_sf_pro,
+                        fontWeight = FontWeight(600),
+                        color = LighterBlack,
+                    )
+                )
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+//                        .padding(16.dp)
+                ) {
+
+                    Text(
+                        text = "Store: Store Name",
+                        color = Color.Black,
+                        textAlign = TextAlign.Start,
+                        fontSize = 13.sp,
+                        modifier = Modifier.weight(0.5f)
+                    )
+
+                    Text(
+                        text = "Receipt Date: 13-07-2023",
+                        color = Color.Black,
+                        textAlign = TextAlign.Start,
+                        fontSize = 13.sp,
+                        modifier = Modifier.weight(0.5f)
+                    )
+                }
+                ReceiptDetailTable()
+
+                Column(modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxHeight(), verticalArrangement = Arrangement.Bottom, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth(), onClick = {
+                        },
+                        colors = ButtonDefaults.buttonColors(VibrantPurple40),
+                        shape = RoundedCornerShape(100.dp)
+
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.button_submit_receipt),
+                            fontFamily = font_sf_pro,
+                            textAlign = TextAlign.Center,
+                            fontSize = 16.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(top = 3.dp, bottom = 3.dp)
+                        )
+                    }
+
+                    Text(
+                        text = stringResource(id = R.string.button_try_again),
+                        fontFamily = font_sf_pro,
+                        modifier = Modifier
+                            .padding(top = 12.dp, bottom = 3.dp),
+                        textAlign = TextAlign.Center,
+                        fontSize = 16.sp,
+                        color = Color(0xFF181818),
+                        fontWeight = FontWeight(400),
+
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ScannedPreview(){
+    ShowScannedReceiptPopup()
+}
