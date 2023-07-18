414e629219ff23d7059359a20a66433f5a8932bf
Akash Agrawal <akash.agrawal@salesforce.com>
2023-07-18 16:32:34 +0530

    adding camera capture.

--- SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/receipts/ReceiptsList.kt
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/receipts/ReceiptsList.kt
@@ -1,5 +1,13 @@
 package com.salesforce.loyalty.mobile.myntorewards.views.receipts
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -19,8 +27,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
@@ -31,10 +41,22 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.salesforce.loyalty.mobile.MyNTORewards.BuildConfig
 import com.salesforce.loyalty.mobile.MyNTORewards.R
 import com.salesforce.loyalty.mobile.myntorewards.ui.theme.*
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,16 +64,61 @@ fun ReceiptsList(navController: NavHostController) {
 
     var text by rememberSaveable { mutableStateOf("") }
     var active by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        BuildConfig.APPLICATION_ID + ".provider", file
+    )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+
+    val cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+            capturedImageUri = uri
+        }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if (it) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(start = 16.dp, end = 16.dp)
-            .background(TextPurpleLightBG)
+            .background(TextPurpleLightBG),
+
+        horizontalAlignment = Alignment.CenterHorizontally
     )  {
 
         Spacer(modifier = Modifier.height(50.dp))
-        Image(
+        //AppContent()
+
+        if (capturedImageUri.path?.isNotEmpty() == true) {
+            Image(
+                modifier = Modifier
+                    .padding(16.dp, 8.dp),
+                painter = rememberImagePainter(capturedImageUri),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+
+            ProcessButton()
+        }
+        else
+        {
+
+       Image(
             painter = painterResource(id = R.drawable.back_arrow),
             contentDescription = "receipt_back_button",
             contentScale = ContentScale.FillWidth,
@@ -74,6 +141,15 @@ fun ReceiptsList(navController: NavHostController) {
                 Button(
                     modifier = Modifier
                         .weight(0.25f), onClick = {
+
+                        val permissionCheckResult =
+                            ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                        if (permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                            cameraLauncher.launch(uri)
+                        } else {
+                            // Request a permission
+                            permissionLauncher.launch(Manifest.permission.CAMERA)
+                        }
 
                     },
                     colors = ButtonDefaults.buttonColors(VibrantPurple40),
@@ -101,8 +177,32 @@ fun ReceiptsList(navController: NavHostController) {
                 }
             }
         }
+
+        }
     }
+
 }
+
+
+/*@Composable
+fun AppContent() {
+
+
+
+
+}*/
+fun Context.createImageFile(): File {
+    // Create an image file name
+    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+    val imageFileName = "JPEG_" + timeStamp + "_"
+    val image = File.createTempFile(
+        imageFileName, /* prefix */
+        ".jpg", /* suffix */
+        externalCacheDir      /* directory */
+    )
+    return image
+}
+
 
 @Composable
 fun ReceiptItem(){
@@ -194,6 +294,32 @@ fun SearchBar(onSearch: (String) -> Unit, modifier: Modifier) {
             unfocusedContainerColor = SearchFieldColor
         )
     )
+}
+
+
+@Composable
+fun ProcessButton() {
+
+    Button(
+        modifier = Modifier
+            .width(288.dp), onClick = {
+
+        },
+        colors = androidx.compose.material3.ButtonDefaults.buttonColors(VibrantPurple40),
+        shape = RoundedCornerShape(100.dp)
+
+    ) {
+        androidx.compose.material3.Text(
+            text = "Process",
+            fontFamily = font_sf_pro,
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 3.dp, bottom = 3.dp)
+        )
+    }
 }
 
 @Preview
