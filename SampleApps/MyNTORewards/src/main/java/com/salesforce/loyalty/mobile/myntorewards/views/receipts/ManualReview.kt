bda6a7624667f9b2700291e236daca5bdb796771
Akash Agrawal <akash.agrawal@salesforce.com>
2023-08-10 23:38:12 +0530

    fixed UI Observation.

--- SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/receipts/ManualReview.kt
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/receipts/ManualReview.kt
@@ -36,8 +36,10 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
 import com.salesforce.loyalty.mobile.MyNTORewards.R
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.LightBlack
 import com.salesforce.loyalty.mobile.myntorewards.ui.theme.LighterBlack
 import com.salesforce.loyalty.mobile.myntorewards.ui.theme.MyProfileScreenBG
+import com.salesforce.loyalty.mobile.myntorewards.ui.theme.TextDarkGray
 import com.salesforce.loyalty.mobile.myntorewards.ui.theme.VibrantPurple40
 import com.salesforce.loyalty.mobile.myntorewards.ui.theme.font_archivo
 import com.salesforce.loyalty.mobile.myntorewards.ui.theme.font_sf_pro
@@ -60,9 +62,9 @@ fun ManualReview( closePopup: (ReceiptListScreenPopupState) -> Unit) {
         Box() {
             Column(
                 modifier = Modifier
-                    .fillMaxHeight(0.6f)
+                    .fillMaxHeight(0.8f)
                     .background(MyProfileScreenBG, RoundedCornerShape(22.dp))
-                    .padding(16.dp)
+                    .padding(start = 20.dp, end = 18.dp, top = 16.dp, bottom = 16.dp)
                     .verticalScroll(
                         rememberScrollState()
                     ),
@@ -71,30 +73,36 @@ fun ManualReview( closePopup: (ReceiptListScreenPopupState) -> Unit) {
             ) {
 
                 Column(
-                    modifier = Modifier.fillMaxWidth().padding(end=16.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 16.dp),
                     horizontalAlignment = Alignment.End
                 ) {
                     Image(
-                        painter = painterResource(id = R.drawable.close_popup_icon),
+                        painter = painterResource(id = R.drawable.close_button_without_bg),
                         contentDescription = stringResource(R.string.cd_close_popup),
                         contentScale = ContentScale.FillWidth,
                         modifier = Modifier
                             .background(Color.White, CircleShape)
-                            .padding(3.dp).clickable{
+                            .clickable {
                                 closePopup(ReceiptListScreenPopupState.RECEIPT_LIST_SCREEN)
-                            }.align(Alignment.End))
+                            }
+                            .align(Alignment.End))
                 }
 
                 Spacer(modifier = Modifier.height(24.dp))
 
                 Text(
-                    text = "Submit for Manual Review",
+                    text = stringResource(id = R.string.text_submit_manual_review),
                     fontWeight = FontWeight.SemiBold,
                     color = LighterBlack,
                     fontFamily = font_sf_pro,
                     textAlign = TextAlign.Start,
                     fontSize = 16.sp,
-                    modifier = Modifier.fillMaxWidth().align(Alignment.Start).padding(start = 8.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Start)
+                        .padding(start = 4.dp))
 
                 Spacer(modifier = Modifier.height(8.dp))
                 Row(
@@ -102,14 +110,14 @@ fun ManualReview( closePopup: (ReceiptListScreenPopupState) -> Unit) {
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(8.dp)
+                        .padding(4.dp)
                 ) {
                     Column(
-                        modifier = Modifier.weight(0.7f),
+                        modifier = Modifier.weight(0.65f),
                         verticalArrangement = Arrangement.spacedBy(4.dp)
                     ) {
                         Text(
-                            text = "Receipt Number",
+                            text = stringResource(id = R.string.receipt_number_text),
                             fontWeight = FontWeight.SemiBold,
                             color = LighterBlack,
                             fontFamily = font_sf_pro,
@@ -150,32 +158,57 @@ fun ManualReview( closePopup: (ReceiptListScreenPopupState) -> Unit) {
                 }
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    text = "Comments",
+                    text = stringResource(id = R.string.comment_text),
                     style = TextStyle(
-                        fontSize = 16.sp,
+                        fontSize = 13.sp,
                         lineHeight = 22.4.sp,
                         fontFamily = font_sf_pro,
                         fontWeight = FontWeight.Normal,
-                        color = LighterBlack,
+                        color = LightBlack,
                         textAlign = TextAlign.Start,
                     ),
-                    modifier = Modifier.fillMaxWidth().align(Alignment.Start)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .align(Alignment.Start)
                 )
                 Spacer(modifier = Modifier.height(8.dp))
 
                 var reviewText by remember { mutableStateOf(TextFieldValue("")) }
                 OutlinedTextField(
                     modifier = Modifier
-                        .border(width = 1.dp, color = Color(0xFFA0A0A0), shape = RoundedCornerShape(size = 8.dp))
-                    .fillMaxWidth().height(120.dp)
-                    .background(color = Color(0xFFFAFCFF), shape = RoundedCornerShape(size = 8.dp)),
-
+                        .border(
+                            width = 1.dp,
+                            color = Color(0xFFA0A0A0),
+                            shape = RoundedCornerShape(size = 8.dp)
+                        )
+                        .fillMaxWidth()
+                        .height(186.dp)
+                        .background(
+                            color = Color(0xFFFAFCFF),
+                            shape = RoundedCornerShape(size = 8.dp)
+                        ),
+                    placeholder= {Text(
+                        text = stringResource(id = R.string.review_screen_placeholder_text),
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            lineHeight = 24.sp,
+                            fontFamily = font_sf_pro,
+                            fontWeight = FontWeight.Normal,
+                            color = TextDarkGray,
+                        )
+                    )},
                     shape = RoundedCornerShape(8.dp),
                     value = reviewText,
                     onValueChange = {reviewText = it},
                     keyboardOptions = KeyboardOptions(imeAction = androidx.compose.ui.text.input.ImeAction.Done),
-                    maxLines = 5,
-                    textStyle = MaterialTheme.typography.caption
+                    maxLines = 7,
+                    textStyle = TextStyle(
+                        fontSize = 16.sp,
+                        lineHeight = 24.sp,
+                        fontFamily = font_sf_pro,
+                        fontWeight = FontWeight.Normal,
+                        color = TextDarkGray,
+                    )
                 )
 
                 Column(
@@ -197,19 +230,19 @@ fun ManualReview( closePopup: (ReceiptListScreenPopupState) -> Unit) {
 
                     ) {
                         Text(
-                            text = "Submit for Manual Review",
+                            text = stringResource(id = R.string.text_submit_manual_review),
                             fontFamily = font_sf_pro,
                             textAlign = TextAlign.Center,
                             fontSize = 16.sp,
                             color = Color.White,
-                            fontWeight = FontWeight.Bold,
+                            fontWeight = FontWeight.SemiBold,
                             modifier = Modifier
                                 .padding(top = 3.dp, bottom = 3.dp)
                         )
                     }
 
                     Text(
-                        text = "Back",
+                        text = stringResource(id = R.string.back_text),
                         fontFamily = font_sf_pro,
                         modifier = Modifier
                             .padding(top = 12.dp, bottom = 3.dp)
