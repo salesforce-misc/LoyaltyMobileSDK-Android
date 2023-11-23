fb10650a00a533a770156f40c7ebb3e8526b0325
Polam Naganjaneyulu <pnaganjaneyulu@salesforce.com>
2023-11-23 13:16:12 +0530

    Create bottom sheet shape

--- SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/components/CustomBottomSheet.kt
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/views/components/CustomBottomSheet.kt
@@ -1,10 +1,14 @@
 package com.salesforce.loyalty.mobile.myntorewards.views.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetState
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+
+val bottomSheetShape = RoundedCornerShape(22.dp, 22.dp, 0.dp, 0.dp)
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
