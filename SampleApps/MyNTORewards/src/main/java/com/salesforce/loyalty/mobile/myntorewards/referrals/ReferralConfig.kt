4edc9368e17ebbb4fc93b0a453496ccb1a0b7a1b
Polam Naganjaneyulu <pnaganjaneyulu@salesforce.com>
2024-01-23 17:51:03 +0530

    Code Refactor

---
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/referrals/ReferralConfig.kt
@@ -0,0 +1,8 @@
+package com.salesforce.loyalty.mobile.myntorewards.referrals
+
+object ReferralConfig {
+    const val REFERRAL_PROGRAM_NAME = "Referral Program"
+    const val REFERRAL_PROMO_CODE = "TEMPRPP"
+    const val REFERRAL_PROGRAM_ID = "0lpB0000000TUjiIAG"
+    const val REFERRAL_DURATION = 90
+}
