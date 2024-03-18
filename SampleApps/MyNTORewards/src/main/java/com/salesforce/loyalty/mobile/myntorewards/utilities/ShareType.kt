9970876c5262fc55a859c6b1a84be1ed3e4386e4
Polam Naganjaneyulu <pnaganjaneyulu@salesforce.com>
2024-03-18 11:13:52 +0530

    code refactor

--- SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/utilities/ShareType.kt
+++ SampleApps/MyNTORewards/src/main/java/com/salesforce/loyalty/mobile/myntorewards/utilities/ShareType.kt
@@ -6,14 +6,14 @@ import com.salesforce.loyalty.mobile.MyNTORewards.R
 /**
  * This class holds the all possible social media sharing options
  * To customise the social media icons, change the iconIds configured below
- * To add new social media sharing option, add it to this enum class with iconId, packageName and Intent share type
+ * To add new sharing option, add below with iconId, packageName and Intent share type
  */
-enum class ShareType(@DrawableRes val iconId: Int, val packageName: String?, val intentShareType: String?) {
+enum class ShareType(@DrawableRes val iconId: Int, val packageName: String?, val intentShareType: String? = ShareType.INTENT_TYPE_TEXT) {
     FACEBOOK(R.drawable.ic_facebook, ShareType.FACEBOOK_APP_PACKAGE, ShareType.INTENT_TYPE_IMAGE),
-    INSTAGRAM(R.drawable.ic_instagram, ShareType.INSTAGRAM_APP_PACKAGE, ShareType.INTENT_TYPE_TEXT),
-    WHATSAPP(R.drawable.ic_whatsapp, ShareType.WHATSAPP_APP_PACKAGE, ShareType.INTENT_TYPE_TEXT),
-    TWITTER(R.drawable.ic_twitter, ShareType.TWITTER_APP_PACKAGE, ShareType.INTENT_TYPE_TEXT),
-    SHARE_OTHERS(R.drawable.ic_share, null, ShareType.INTENT_TYPE_TEXT); // Package name is null as its generic sharing option
+    INSTAGRAM(R.drawable.ic_instagram, ShareType.INSTAGRAM_APP_PACKAGE),
+    WHATSAPP(R.drawable.ic_whatsapp, ShareType.WHATSAPP_APP_PACKAGE),
+    TWITTER(R.drawable.ic_twitter, ShareType.TWITTER_APP_PACKAGE),
+    SHARE_OTHERS(R.drawable.ic_share, null); // Package name is null as its generic sharing option
 
     companion object {
         const val INTENT_TYPE_TEXT = "text/plain"
